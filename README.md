9333789fb024784e0c67b2cb6385ee25a7da7979
Fabrice DUJARDIN <44850811+fabricedujardinportfolio@users.noreply.github.com>
2021-11-02 08:38:37 +1100

    début de ECF pour la CCI de Nouméa

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Ecf-Optimum-Fabrice
 Ecf cci pour le plugin de réservation
+
+
+## Procédure à suivre pour installer le projet
---
+++ hello.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package Optimum Réservation
+ * @version 1.0
+ */
+/*
+Plugin Name: Optimum Réservation
+Description: These just a booking plugin for ECF CCI in Nouméa
+Author: Fabrice DUJARDIN
+Version: 1.0
+Author URI: https://fabricedujardinportfolio.github.io/
+*/
+
